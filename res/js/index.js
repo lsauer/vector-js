89c93a6a72547deec1bd01e46aa5e2f8c9648087
lsauer <brainbrawnheart@gmail.com>
2013-04-22 21:16:54 +0200

    added: show the relative magnitude of each vector compared to the longest vector

--- res/js/index.js
+++ res/js/index.js
@@ -26,15 +26,36 @@
 					,arrvects[1].spherical().p * vectorop.RADIAN
 					,arrvects[2].spherical().p * vectorop.RADIAN
 				);
-				//collect the set operators from the TryIt Demo
+				var arranglstheta = new Array(
+					 arrvects[0].spherical().t * vectorop.RADIAN
+					,arrvects[1].spherical().t * vectorop.RADIAN
+					,arrvects[2].spherical().t * vectorop.RADIAN
+				);				//collect the set operators from the TryIt Demo
+				
+				//show normalized magnitude
+				var arrlengths = new Array(
+				   arrvects[0].length
+				  ,arrvects[1].length
+				  ,arrvects[2].length
+				);
+				var maxlength = Math.max.apply(null, arrlengths);
+				$('div[name=relvectormagnitude]').each(
+				  function(i, e){
+					  //children() function in jQuery-elements
+					  e.children[0].style.width = (arrlengths[i]/maxlength)*100+'%'; 
+				  }
+				);
+				
+				
 				var arrops =[];
 				$('.vectorop').each( function(i,e){arrops.push(e.value);} )
 				
 				//calculate and set the angle of the vector in 2d; theta-angle and 3d projection are disregarded
-				 $('.resultarrow1').css( {'-webkit-transform':'rotate(' + arranglsphi[0] +'deg)'} ); 
-				 $('.resultarrow2').css( {'-webkit-transform':'rotate(' + arranglsphi[1] +'deg)'} ); 
-				 $('.resultarrow3').css( {'-webkit-transform':'rotate(' + arranglsphi[2] +'deg)'} ); 
-				 				
+				//use rotateY to give certain perspective
+				 $('.resultarrow1').css( {'-webkit-transform':'rotate(' + arranglsphi[0] +'deg) rotateY(' + arranglstheta[0]/2 +'deg)'} ); 
+				 $('.resultarrow2').css( {'-webkit-transform':'rotate(' + arranglsphi[1] +'deg) rotateY(' + arranglstheta[1]/2 +'deg)'} ); 
+				 $('.resultarrow3').css( {'-webkit-transform':'rotate(' + arranglsphi[2] +'deg) rotateY(' + arranglstheta[2]/2 +'deg)'} ); 
+				 
 				//calculate the result
 				var calcresult =eval('('+arrops[0] + (+arrvects[0])
 										+arrops[1] + (+arrvects[1]) 
@@ -46,7 +67,9 @@
 				);
 				 
 				$('#calcResult').attr('value', vectresult.toString(true) );
-				$('.resultarrow4').css( {'-webkit-transform':'rotate(' + (vectresult.spherical().p * vectorop.RADIAN) +'deg)'} ); 
+				$('.resultarrow4').css( {'-webkit-transform':'rotate(' + (vectresult.spherical().p * vectorop.RADIAN) +'deg)\
+										 rotateY(' + (vectresult.spherical().t * vectorop.RADIAN)/2 +'deg)'} 
+				); 
 			}
 		);
 		
